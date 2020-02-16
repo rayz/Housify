137102f02b48d79ce783b1640918e1db20e30e92
raymond zhao <raymnzd@gmail.com>
2020-02-16 03:56:00 -0500

    house copied to clipboard

--- housify.py
+++ housify.py
@@ -1,24 +1,31 @@
+import pyperclip
 from math import sqrt
 
 def housify(text):
+    res = []
     text = text.upper().replace(" ", "")
     nearest_square = round(sqrt(len(text))) ** 2
     letters_perline = int(sqrt(nearest_square))
     t_len = len(text)
     index = 0
+
     for i in range(t_len // letters_perline):
-        print(text[index:index+letters_perline])
+        res.append(text[index:index+letters_perline])
         index += letters_perline
         t_len -= letters_perline
+    last_line = text[index:]
 
-    print(text[index:], end = "")
     if t_len > 0:
-        print("." * (letters_perline - t_len))
+        last_line += "." * (letters_perline - t_len)
 
+    res.append(last_line)
+    pyperclip.copy("\n".join(res))
+
+    print("\n".join(res), end = "")
+    print("House successfully copied to clipboard!")
 
 def main():
     housify(input("Enter text to housify: "))
-
 
 if __name__ == "__main__":
     main()
